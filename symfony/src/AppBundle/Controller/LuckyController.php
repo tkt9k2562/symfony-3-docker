6f76b0b4121225029fdc02039c1a548021fc0135
tkt9k2562 <tkt9k2562@gmail.com>
2020-10-11 13:43:03 +0800

    add lucky_number controller

---
+++ symfony/src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController
+{
+    /**
+     * @Route("/lucky/number")
+     */
+    public function numberAction()
+    {
+        $number = rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: ' . $number . '</body></html>'
+        );
+    }
+}
